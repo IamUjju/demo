95c49ffe8b886ed38a5617dc1b880a8d592b9379
Ujjawal Kumar <kujjawal623@gmail.com>
2023-11-10 13:30:31 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+    const year = new Date().getFullYear();
+  return (
+    <div>
+        <p className='footer'>Copyright © {year}</p>
+    </div>
+  )
+}
+
+export default Footer
